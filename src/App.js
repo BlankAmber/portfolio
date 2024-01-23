1b7bf084d7dbd92635c7babd174dd44d2bb98627
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2024-01-23 10:36:42 +0000

    Replace some default HTML

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Work in progress...</p>
+        <h1>By Samuel! :)</h1>
       </header>
     </div>
   );
